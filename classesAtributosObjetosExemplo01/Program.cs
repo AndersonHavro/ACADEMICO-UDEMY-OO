9d6d10b193c6589a78cba872d24bcdf0d7f41e4a
Anderson Havro <andersonhavro@gmail.com>
2019-08-28 16:18:43 -0300

    exemplo01

---
+++ classesAtributosObjetosExemplo01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+namespace classesAtributosObjetosExemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do triangulo X: \n");
+            x.LadoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.LadoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.LadoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do triangulo Y: \n");
+            y.LadoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.LadoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.LadoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Area de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            char maiorArea = 'X';
+            if (areaY > areaX)
+            {
+                maiorArea = 'Y';
+            }
+            Console.WriteLine("Maior area: " + maiorArea);
+
+        }
+    }
+}
---
+++ classesAtributosObjetosExemplo01/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classesAtributosObjetosExemplo01
+{
+    public class Triangulo
+    {
+        public double LadoA { get; set; }
+        public double LadoB { get; set; }
+        public double LadoC { get; set; }
+
+        public double Area()
+        {
+            double p = (LadoA + LadoB + LadoC) / 2;
+            return  Math.Sqrt(p * (p - LadoA) * (p- LadoB) * (p - LadoC));
+        }
+    }
+}
