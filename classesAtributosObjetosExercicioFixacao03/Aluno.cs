42294e45bdd61ab3a4b93c19b403bd5b14bb6695
Anderson Havro <andersonhavro@gmail.com>
2019-08-29 18:01:17 -0300

    classesAtributosObjetosExercicioFixacao03

---
+++ classesAtributosObjetosExercicioFixacao03/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace classesAtributosObjetosExercicioFixacao03
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+        public double Media()
+        {
+            return (Nota1 + Nota2 + Nota3) ;
+        }
+        public string Resultado()
+        {
+            string result = "reprovado";
+            if (Media() >= 60)
+                result = "aprovado";
+            return result;
+        }
+        public double Resto()
+        {
+            double resto = 0;
+            if (Media() < 60)
+                resto = 60 - Media();
+            return resto;
+        }
+
+
+    }
+
+
+}
+
---
+++ classesAtributosObjetosExercicioFixacao03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace classesAtributosObjetosExercicioFixacao03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.WriteLine("Notas: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            aluno.Nota2 = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            aluno.Nota3 = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Nota Final: "+aluno.Media().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(aluno.Resultado());
+            if (aluno.Resultado()=="reprovado")
+            {
+                Console.WriteLine("Faltam "+aluno.Resto().ToString("F2", CultureInfo.InvariantCulture) 
+                    + " pontos");
+            }
+           
+            
+        }
+    }
+}
