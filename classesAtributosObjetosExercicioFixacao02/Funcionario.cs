e838d5df4dbaa80462782651fe1a54d4a26c3299
Anderson Havro <andersonhavro@gmail.com>
2019-08-29 17:00:55 -0300

    classesAtributosObjetosExercicioFixacao02

---
+++ classesAtributosObjetosExercicioFixacao02/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace classesAtributosObjetosExercicioFixacao02
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double aumento)
+        {
+            SalarioBruto = SalarioBruto*((aumento/100)+1); 
+        }
+
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ classesAtributosObjetosExercicioFixacao02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+/*
+ * Fazer um programa para ler os dados de um funcionário (nome,
+salário bruto e imposto). Em seguida, mostrar os dados do
+funcionário (nome e salário líquido). Em seguida, aumentar o salário
+do funcionário com base em uma porcentagem dada (somente o
+salário bruto é afetado pela porcentagem) e mostrar novamente os
+dados do funcionário. Use a classe projetada abaixo.
+*/
+namespace classesAtributosObjetosExercicioFixacao02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+            Console.WriteLine("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine("Salario: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Funcionario: "+f);
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salario: ");
+            double aumento = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            f.AumentarSalario(aumento);
+            Console.WriteLine("Dados atualizados: " + f);
+
+
+        }
+    }
+}
