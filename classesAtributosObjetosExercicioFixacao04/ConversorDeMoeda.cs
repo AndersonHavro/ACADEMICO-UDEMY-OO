9266afbd19a275c0561b0ebf517cc533cda9fc78
Anderson Havro <andersonhavro@gmail.com>
2019-08-29 18:44:07 -0300

    classesAtributosObjetosExercicioFixacao04

---
+++ classesAtributosObjetosExercicioFixacao04/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classesAtributosObjetosExercicioFixacao04
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 6;
+
+        public static double Conversao(double cotacao, double quant)
+        {
+            return (((IOF / 100) + 1) * (cotacao * quant));
+        }
+    }
+}
---
+++ classesAtributosObjetosExercicioFixacao04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace classesAtributosObjetosExercicioFixacao04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação em dolar? ");
+            double cotacao = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            Console.Write("Quantos dolares voce vai comprar? ");
+            double quant = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor a ser pago em reais: "
+                                       +ConversorDeMoeda.Conversao(cotacao,quant).ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
