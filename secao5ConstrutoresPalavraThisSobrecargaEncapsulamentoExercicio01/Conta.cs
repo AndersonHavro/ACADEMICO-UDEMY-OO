36fa1728dc6c2dc6551182e881fc22492a6b0dd4
Anderson Havro <andersonhavro@gmail.com>
2019-08-30 16:36:28 -0300

    secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01

---
+++ secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Conta(int numero, string nome, double depInicial)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo += depInicial;
+        }
+
+        public void Deposito(double valorDep)
+        {
+            Saldo += valorDep;
+        }
+        public void Saque(double valorSaque)
+        {
+
+             Saldo = (-1*(valorSaque + 5)) + Saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Nome + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
--- secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01/Program.cs
+++ secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01
 {
@@ -6,8 +7,33 @@ namespace secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("oi");
-            _ = Console.ReadLine();
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial(s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            Conta conta;
+            if (resposta == 'n')
+                conta = new Conta(numero, nome);
+            else
+            {
+                Console.Write("Infomre o valor do depósito: ");
+                double depInicial = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+                conta = new Conta(numero, nome, depInicial);
+            }
+            Console.WriteLine("Dados da conta: \n" + conta);
+            Console.WriteLine("");
+            Console.Write("Digite um valor para depósito: ");
+            double valorDep = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            conta.Deposito(valorDep);
+            Console.WriteLine("Dados atualzados: \n" + conta);
+            Console.WriteLine("");
+            Console.Write("Digite um valor do saque: ");
+            double valorSaque = double.Parse(Console.ReadLine().ToString(CultureInfo.InvariantCulture));
+            conta.Saque(valorSaque);
+            Console.WriteLine("Dados atualzados: \n" + conta);
+
         }
     }
 }
