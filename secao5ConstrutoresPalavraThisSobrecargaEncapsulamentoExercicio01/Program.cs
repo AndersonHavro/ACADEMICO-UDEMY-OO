5b465c4951d62a2b9f7abc5fec3fb97b18e1710e
Anderson Havro <andersonhavro@gmail.com>
2019-08-30 12:37:15 -0300

    teste

---
+++ secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace secao5ConstrutoresPalavraThisSobrecargaEncapsulamentoExercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("oi");
+            _ = Console.ReadLine();
+        }
+    }
+}
