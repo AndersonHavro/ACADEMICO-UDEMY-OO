0ed711d32660452812cae30f6b3e1e3b93b15c0b
Anderson Havro <andersonhavro@gmail.com>
2019-08-31 19:51:30 -0300

    exemplos 1 e 2

---
+++ Seção_6ArrayListasExempo2/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seção_6ArrayListasExempo2
+{
+    class Product
+    {
+        public string Nome { get; set; }
+        public double Price { get; set; }
+
+        public Product(string nome, double price)
+        {
+            Nome = nome;
+            Price = price;
+        }
+    }
+}
