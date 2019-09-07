18fda0857737ef218c89e39a6891b95ceaf23b75
AndersonHavro <andersonhavro@gmail.com>
2019-09-07 08:24:20 -0300

    exemplo Curso Enumeracoes

---
+++ Secao9Enumerações/Entites/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Secao9Enumerações.Entites.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
---
+++ Secao9Enumerações/Entites/Order.cs
@@ -0,0 +1,19 @@
+﻿using Secao9Enumerações.Entites.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao9Enumerações.Entites
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + "," + Moment + "," + Status;
+        }
+    }
+}
