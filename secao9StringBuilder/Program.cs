037c458f7293cf21d606a22099d46fef67b2a300
AndersonHavro <andersonhavro@gmail.com>
2019-10-30 16:08:29 -0300

    stringbuilder

---
+++ secao9StringBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿using secao9StringBuilder.Entites;
+using System;
+
+namespace secao9StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment comment01 = new Comment("Have a nice trip!!!");
+            Comment comment02 = new Comment("Wow that's awesome.");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit Austrtalia", 2
+                );
+            p1.AddComment(comment01);
+            p1.AddComment(comment02);
+
+
+
+        }
+    }
+}
---
+++ secao9StringBuilder01/Entites/Post.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace secao9StringBuilder01.Entites
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {
+            StringBuilder st = new StringBuilder();
+            st.AppendLine(Title);
+            st.Append(Likes);
+            st.Append("Likes - ");
+            st.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            st.AppendLine(Content);
+            st.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                st.AppendLine(c.Text);
+            }
+ 
+           
+            return st.ToString();
+
+        }
+    }
+}
